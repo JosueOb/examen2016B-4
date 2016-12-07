fdb80453001181259d22f957a9025469270fdcc9
Usuario01-PC\Usuario01 <bryan_adri_94@outlook.com>
2016-12-07 13:21:39 -0500

    conteo de palabras

---
+++ conteo.py
@@ -0,0 +1,28 @@
+def creartxt():
+    archi=open('conteo.txt','w')
+    archi.close()
+    
+def grabartxt(a):
+    archi=open('conteo.txt','a')
+    archi.write("El Total de la palbra Bryan son:\t")
+    archi.write(str(a))
+    archi.close() 
+
+
+def leertxt():
+    b=0
+    archi=open('integrantes1000kb.txt','r')
+    linea=archi.readline()
+    while linea!="":
+        print(linea)
+        for i in range(len(linea)):
+            if linea[i] =="Pilatuña":
+                b=b+1
+        a=len(linea)-b
+        linea=archi.readline()
+               
+    archi.close()
+    grabartxt(a)
+        
+creartxt()       
+leertxt()

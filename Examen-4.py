ee2d921dde52fcd9099b5ec0b19af4e15392a241
USUARIO07-PC\Usuario01 <lizeth.toasa@gmail.com>
2016-12-07 13:28:26 -0500

    Creacion de menu principal y forma rectangular de la piscina

--- Examen-4.py
+++ Examen-4.py
@@ -7,3 +7,20 @@
 
 #FECHA:07/12/2016
 
+import sys
+
+def menu_princ():
+    print("\t\tESCUELA POLITECNICA NACIONAL")
+    print("\t\tESCUELA DE FORMACION DE TECNOLOGOS")
+    print("\t\t***MENU PRINCIPAL***")
+    print("1.-Archivos con nombres")
+    print("2.-Archivos con repeticiones")
+    print("3.-Volumen de una piscina")
+    print("4.-Salir")
+
+def rect():
+    largo=float(input("Ingrese el largo: "))
+    ancho=float(input("Ingrese el ancho: "))
+    prof=float(input("Ingrese la profundidad: "))
+    volumen=largo*ancho*prof
+    print("El volumen de forma rectagular es:",volumen)

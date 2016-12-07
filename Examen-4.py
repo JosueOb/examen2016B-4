3c4df3019d7feed093a02d6fc91fca4f40e7712f
USUARIO08-PC\Usuario01 <chulded@gmail.com>
2016-12-07 13:33:01 -0500

    Creacion menu piscina y forma eliptica de la piscina

--- Examen-4.py
+++ Examen-4.py
@@ -18,9 +18,28 @@ def menu_princ():
     print("3.-Volumen de una piscina")
     print("4.-Salir")
 
+def menu_piscina():
+    print("\t**CALCULO DEL VOLUMEN DE UNA PISCINA**")
+    print("1.-Forma rectangular ")
+    print("2.-Forma eliptica")
+    print("3.-Forma circular")
+    print("4.-Regresar al Menu Principal")
+
 def rect():
     largo=float(input("Ingrese el largo: "))
     ancho=float(input("Ingrese el ancho: "))
     prof=float(input("Ingrese la profundidad: "))
     volumen=largo*ancho*prof
     print("El volumen de forma rectagular es:",volumen)
+
+def elip():
+    largo=float(input("Ingrese el largo: "))
+    ancho=float(input("Ingrese el ancho: "))
+    prof=float(input("Ingrese la profundidad: "))
+    pi=3.1416
+    volumen=(4/3)*pi*largo*ancho*prof
+    print("El volumen de forma eliptica es:",volumen)
+
+
+
+
